ed85ddd9dbb0fafb0e3783b89554b28274206b62
oussamale <oussama.lekhal20@gmail.com>
2024-05-27 14:09:27 +0100

    Update badrsama.sql

--- badrsama.sql
+++ badrsama.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mer. 22 mai 2024 à 16:35
+-- Généré le : lun. 27 mai 2024 à 15:08
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -53,15 +53,17 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`CategoryID`, `CategoryName`, `Description`, `CategoryImage`) VALUES
+(1, 'Foods', 'For you and your family and me', '664f3b1e92bd3_IMG_7832.jpg'),
 (2, 'Supermaker', 'Supermaker', '664dec97816e2_664dc70608734_superMaker.jpg'),
 (3, 'Health & Beauty', 'Health & Beauty', '664dcbbbc148a_Health & Beauty.jpg'),
 (4, 'Home & Office', 'Home & Office', '664dcbdb47b9f_Home & Office.jpg'),
-(5, 'Appliance', 'Appliance', '664dcc14ebe87_Appliance.jpg'),
+(5, 'Appliance', 'Appliance', '664f388454ffb_fY0wJ-QaIKghQ87Qs9ufsshTAws.png'),
 (6, 'Phone & Tablets', 'Phone & Tablets', '664dcc2944b3f_Phone & Tablets.jpg'),
 (7, 'Computing', 'Computing', '664dcc41469ea_Computing.jpg'),
 (8, 'Electronics', 'Electronics', '664dcc632d7d8_Electronics.jpg'),
 (9, 'Fashion', 'Fashion', '664dcc8d6da6d_Fashion.jpg'),
-(10, 'Baby Products', 'Baby Products', '664dd311b5355_jomjakkapat-parrueng-qaUMOLJwb48-unsplash.jpg');
+(10, 'Baby Products', 'Baby Products', '664dd311b5355_jomjakkapat-parrueng-qaUMOLJwb48-unsplash.jpg'),
+(11, 'carte graphic', 'qsdfghyjkl', '66531ff8ea484_8083c857-f3ca-48a8-97cd-ace3b8666886.png');
 
 -- --------------------------------------------------------
 
@@ -122,6 +124,36 @@ CREATE TABLE `productattributes` (
   `DataType` enum('string','integer','decimal','boolean','date') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+--
+-- Déchargement des données de la table `productattributes`
+--
+
+INSERT INTO `productattributes` (`AttributeID`, `ProductTypeID`, `AttributeName`, `DataType`) VALUES
+(10, 1, '1', 'integer'),
+(12, 1, '5 dfgh', 'date'),
+(53, 2, 'lovr', 'string'),
+(54, 2, 'dq', 'decimal'),
+(61, 2, 'qsc', 'string'),
+(62, 2, 'ssqqdds', 'decimal'),
+(64, 2, 'lazaza', 'string'),
+(66, 1, 'love', 'string'),
+(67, 3, 'Int', 'integer'),
+(68, 3, 'deci', 'decimal'),
+(69, 3, 'booo', 'boolean'),
+(70, 3, 'date', 'date'),
+(71, 3, 'String', 'string'),
+(72, 4, 'dsqdsq', 'string'),
+(74, 4, 'er', 'date'),
+(75, 5, 'sgdsf', 'date'),
+(76, 5, 'qfsqfq', 'integer'),
+(77, 5, 'qsf', 'date'),
+(78, 1, 'xwwxf', 'boolean'),
+(79, 6, 'str', 'string'),
+(80, 6, 'int', 'integer'),
+(81, 6, 'deci', 'decimal'),
+(82, 6, 'bool', 'boolean'),
+(83, 6, 'date', 'date');
+
 -- --------------------------------------------------------
 
 --
@@ -139,16 +171,20 @@ CREATE TABLE `productattributevalues` (
   `ValueDate` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
--- --------------------------------------------------------
-
 --
--- Structure de la table `productcategories`
+-- Déchargement des données de la table `productattributevalues`
 --
 
-CREATE TABLE `productcategories` (
-  `ProductID` int(11) NOT NULL,
-  `CategoryID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+INSERT INTO `productattributevalues` (`AttributeValueID`, `ProductID`, `AttributeID`, `ValueString`, `ValueInteger`, `ValueDecimal`, `ValueBoolean`, `ValueDate`) VALUES
+(4, 5, 75, NULL, NULL, NULL, NULL, '2024-05-08'),
+(5, 5, 76, NULL, 2, NULL, NULL, NULL),
+(6, 5, 77, NULL, NULL, NULL, NULL, '2024-06-09'),
+(7, 6, 72, 'fjhgfdsgh,jgfdhj', NULL, NULL, NULL, NULL),
+(8, 6, 74, NULL, NULL, NULL, NULL, '2024-06-01'),
+(9, 7, 67, NULL, 111, NULL, NULL, NULL),
+(10, 7, 68, NULL, 222, NULL, NULL, NULL),
+(11, 7, 70, NULL, NULL, NULL, NULL, '2024-05-30'),
+(12, 7, 71, 'DEZA', NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -163,10 +199,53 @@ CREATE TABLE `products` (
   `Price` decimal(10,2) NOT NULL,
   `Stock` int(11) NOT NULL,
   `ProductImage` text NOT NULL,
+  `ProductTypeID` int(11) DEFAULT NULL,
   `CreatedAt` timestamp NOT NULL DEFAULT current_timestamp(),
   `UpdatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `IsDeleted` tinyint(1) NOT NULL
+  `IsDeleted` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Déchargement des données de la table `products`
+--
+
+INSERT INTO `products` (`ProductID`, `Name`, `Description`, `Price`, `Stock`, `ProductImage`, `ProductTypeID`, `CreatedAt`, `UpdatedAt`, `IsDeleted`) VALUES
+(1, '1312', '123456', 234.00, 26, '6653c1451219f_IMG_7830.jpg', 6, '2024-05-26 23:09:57', '2024-05-26 23:09:57', 0),
+(2, 'qqsssqsqs', 'dfgh;,nbvc', 0.32, 20, '6653c985c83b2_a4b524ee-6492-493b-adf8-4fc850079bbb.jpeg', 1, '2024-05-26 23:45:09', '2024-05-26 23:45:09', 0),
+(3, 'qqsssqsqs', 'dfgh;,nbvc', 0.32, 20, '6653cb7aaecd7_a4b524ee-6492-493b-adf8-4fc850079bbb.jpeg', 1, '2024-05-26 23:53:30', '2024-05-26 23:53:30', 0),
+(4, 'qqsssqsqs', 'dfgh;,nbvc', 0.32, 20, '6653ccfe8b837_a4b524ee-6492-493b-adf8-4fc850079bbb.jpeg', 1, '2024-05-26 23:59:58', '2024-05-26 23:59:58', 0),
+(5, 'dwfd', 'sfdsfsfs', 0.01, 1, '6653ce0696b99_4e1d9222-059b-4b0a-a820-7b9a7f6e84a0.png', 5, '2024-05-27 00:04:22', '2024-05-27 00:04:22', 0),
+(6, 'zae', 'aezaz', 0.03, 1, '6653cf521023b_IMG_7830.jpg', 4, '2024-05-27 00:09:54', '2024-05-27 00:09:54', 0),
+(7, 'sqdq', 'dqsdqq', 0.05, 4, '66542781c936e_Greenskull_apartment_micro_black_hole_in_the_center_of_the_room_dd3f62e5-c060-435d-a2c8-4ccfaa11c84f.png', 3, '2024-05-27 06:26:09', '2024-05-27 06:26:09', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `producttypecategories`
+--
+
+CREATE TABLE `producttypecategories` (
+  `ProductTypeID` int(11) NOT NULL,
+  `CategoryID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Déchargement des données de la table `producttypecategories`
+--
+
+INSERT INTO `producttypecategories` (`ProductTypeID`, `CategoryID`) VALUES
+(1, 7),
+(1, 9),
+(1, 10),
+(2, 4),
+(3, 1),
+(3, 5),
+(4, 2),
+(4, 5),
+(5, 1),
+(5, 2),
+(5, 3),
+(5, 5);
 
 -- --------------------------------------------------------
 
@@ -178,6 +257,18 @@ CREATE TABLE `producttypes` (
   `ProductTypeID` int(11) NOT NULL,
   `TypeName` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Déchargement des données de la table `producttypes`
+--
+
+INSERT INTO `producttypes` (`ProductTypeID`, `TypeName`) VALUES
+(4, 'bvc'),
+(3, 'eee'),
+(6, 'love and care'),
+(1, 'test'),
+(5, 'test here'),
+(2, 'yutre');
 
 -- --------------------------------------------------------
 
@@ -283,17 +374,18 @@ ALTER TABLE `productattributevalues`
   ADD KEY `AttributeID` (`AttributeID`);
 
 --
--- Index pour la table `productcategories`
---
-ALTER TABLE `productcategories`
-  ADD PRIMARY KEY (`ProductID`,`CategoryID`),
-  ADD KEY `CategoryID` (`CategoryID`);
-
---
 -- Index pour la table `products`
 --
 ALTER TABLE `products`
-  ADD PRIMARY KEY (`ProductID`);
+  ADD PRIMARY KEY (`ProductID`),
+  ADD KEY `fk_ProductTypeID` (`ProductTypeID`);
+
+--
+-- Index pour la table `producttypecategories`
+--
+ALTER TABLE `producttypecategories`
+  ADD PRIMARY KEY (`ProductTypeID`,`CategoryID`),
+  ADD KEY `CategoryID` (`CategoryID`);
 
 --
 -- Index pour la table `producttypes`
@@ -332,7 +424,7 @@ ALTER TABLE `cartitems`
 -- AUTO_INCREMENT pour la table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `CategoryID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `CategoryID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT pour la table `orderdetails`
@@ -356,25 +448,25 @@ ALTER TABLE `payments`
 -- AUTO_INCREMENT pour la table `productattributes`
 --
 ALTER TABLE `productattributes`
-  MODIFY `AttributeID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `AttributeID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=84;
 
 --
 -- AUTO_INCREMENT pour la table `productattributevalues`
 --
 ALTER TABLE `productattributevalues`
-  MODIFY `AttributeValueID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `AttributeValueID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT pour la table `products`
 --
 ALTER TABLE `products`
-  MODIFY `ProductID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ProductID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT pour la table `producttypes`
 --
 ALTER TABLE `producttypes`
-  MODIFY `ProductTypeID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ProductTypeID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT pour la table `users`
@@ -432,11 +524,17 @@ ALTER TABLE `productattributevalues`
   ADD CONSTRAINT `productattributevalues_ibfk_2` FOREIGN KEY (`AttributeID`) REFERENCES `productattributes` (`AttributeID`);
 
 --
--- Contraintes pour la table `productcategories`
+-- Contraintes pour la table `products`
 --
-ALTER TABLE `productcategories`
-  ADD CONSTRAINT `productcategories_ibfk_1` FOREIGN KEY (`ProductID`) REFERENCES `products` (`ProductID`),
-  ADD CONSTRAINT `productcategories_ibfk_2` FOREIGN KEY (`CategoryID`) REFERENCES `categories` (`CategoryID`) ON DELETE CASCADE;
+ALTER TABLE `products`
+  ADD CONSTRAINT `fk_ProductTypeID` FOREIGN KEY (`ProductTypeID`) REFERENCES `producttypes` (`ProductTypeID`) ON DELETE SET NULL;
+
+--
+-- Contraintes pour la table `producttypecategories`
+--
+ALTER TABLE `producttypecategories`
+  ADD CONSTRAINT `producttypecategories_ibfk_1` FOREIGN KEY (`ProductTypeID`) REFERENCES `producttypes` (`ProductTypeID`),
+  ADD CONSTRAINT `producttypecategories_ibfk_2` FOREIGN KEY (`CategoryID`) REFERENCES `categories` (`CategoryID`);
 
 --
 -- Contraintes pour la table `wishlistitems`
