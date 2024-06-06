5563ea05c8ad6c042dde7fcc2fec08be05d3aea1
oussamale <oussama.lekhal20@gmail.com>
2024-06-06 18:00:36 +0100

    Update session_test.php

--- includes/session_test.php
+++ includes/session_test.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
     if(!isset($_SESSION['id']) && !isset($_SESSION['username'])){
-        header("Location: ../../login/");
+        header("Location: ../login/");
     }
 ?>
